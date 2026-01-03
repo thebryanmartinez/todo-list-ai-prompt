279a0f215e30aa43510b068c3dc486e215ff0963
Bryan Martínez <bryanmartz972@gmail.com>
2026-01-03 14:47:31 -0600

    chore: remove daisyui and add fonts for application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className='flex min-h-screen items-center justify-centerfont-sans '>
-      <button className='btn btn-primary'>Click me</button>
+    <div className='flex min-h-screen items-center justify-center'>
+      <h1>This is a heading example</h1>
+      <span>This is a test of the text</span>
     </div>
   )
 }
